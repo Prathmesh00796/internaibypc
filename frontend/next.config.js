fce9c8ed6c0484236cfef0d22aa166a368573784
Prathmesh Chopade <prathmeshchopade96@gmail.com>
2026-08-02 12:02:31 +0530

    Fix frontend deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,14 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/api/backend/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/v1/:path*`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
