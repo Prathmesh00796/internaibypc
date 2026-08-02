a87feb0014cdf80d1753f2e2feb1dcf7666b0c7b
Prathmesh Chopade <prathmeshchopade96@gmail.com>
2026-08-02 12:52:12 +0530

    Fix app init

--- backend/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .profile import Profile, Skill, Project, Experience
-from .resume import Resume
-from .application import Application
-from .audit_log import AuditLog
-from .notification import Notification
-from .platform_connection import PlatformConnection
